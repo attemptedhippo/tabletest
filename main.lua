bee1966d8168da38917f864df5447eee0efc8210
Shiina <attemptedhippo@gmail.com>
2024-05-19 01:20:39 -0400

    added ui and made scene nodes and tree

--- main.lua
+++ main.lua
@@ -1,11 +1,23 @@
+local suit = require("suit")
+
+local input = { text = "" }
+
 -- Define the N-ary tree node
 local Node = {}
 Node.__index = Node
 
-function Node.new(data1, data2)
+local MyColors = {}
+MyColors.Red = { 1, 0, 0 }
+MyColors.Blue = { 0, 0, 1 }
+MyColors.Yellow = { 1, 1, 0 }
+MyColors.Green = { 0, 1, 0 }
+
+function Node.new(data1, data2, data3, data4)
 	local self = setmetatable({}, Node)
 	self.data1 = data1
 	self.data2 = data2
+	self.data3 = data3
+	self.data4 = data4
 	self.children = {} -- Store child nodes
 	return self
 end
@@ -27,21 +39,17 @@ function Node:removeChild(child)
 end
 
 -- Example usage
-local root = Node.new("\nRoot", "Red")
-local child1 = Node.new("Child 1", "Blue")
-local child2 = Node.new("Child 2", "Blue")
-local child3 = Node.new("Child 3", "Yellow")
-local child4 = Node.new("Child 4", "Green")
-local child5 = Node.new("Child 5", "Green")
-local child6 = Node.new("Child 6", "Yellow")
-local child7 = Node.new("Child 7", "Yellow")
+-- root = Node.new((id)"Root", (bool)traveled, (bool)currentlyHere, (enum?)"location"(inn/forrest/fight), (image)background, (string(or table of strings?))dialogue)
+local root = Node.new("Root", "Red", "Sample_fantasy.png", "Hello world")
+local child1 = Node.new("Child1", "Blue", "Sample_fantasy.png", "Child1")
+local child2 = Node.new("Child2", "Blue", "Sample_urban.png", "Child2")
+local child3 = Node.new("Child3", "Yellow", "Sample_interior.png", "Child3")
 
 root:addChild(child1)
 root:addChild(child2)
 child2:addChild(child3)
-child3:addChild(child4)
-child3:addChild(child5)
-child1:addChild(child6)
+
+local CurrentNode = root
 
 -- Print the tree (depth-first traversal)
 local function printTree(node, depth)
@@ -52,6 +60,48 @@ local function printTree(node, depth)
 	end
 end
 
+local function drawDial(node)
+	local panelHeight = 150
+	local offsetSides = love.graphics.getWidth() / 5 --) - 20
+	-- local offsetTop = love.graphics.getHeight() / 5
+	local offsetTop = love.graphics.getHeight() - panelHeight - 20
+	love.graphics.setColor(love.math.colorFromBytes(44, 47, 51))
+	love.graphics.rectangle("fill", offsetSides - 20, offsetTop, offsetSides * 3 + 40, panelHeight)
+
+	local tempString = CurrentNode.data2
+	love.graphics.setColor(MyColors[tempString])
+	love.graphics.print(node.data4, love.graphics.getWidth() / 2, 200)
+end
+
+local function drawTree(node)
+	local img = love.graphics.newImage(node.data3)
+	-- love.graphics.draw(img, 0, 0)
+	drawDial(node)
+end
+
 function love.load()
+	print("\n")
 	printTree(root)
+end
+
+function love.update(dt)
+	suit.layout:reset(0, 500)
+	suit.layout:col(love.graphics.getWidth() / 5, 30)
+	if suit.Button("Bueton", suit.layout:col()).hit then
+		CurrentNode = CurrentNode.children[1]
+	end
+	suit.layout:col()
+	if suit.Button("Bueton2", suit.layout:col()).hit then
+		CurrentNode = CurrentNode.children[2]
+	end
+	suit.layout:col()
+end
+
+function love.draw()
+	drawTree(CurrentNode)
+	suit.draw()
+end
+
+function love.keypressed(key)
+	suit.keypressed(key)
 end
